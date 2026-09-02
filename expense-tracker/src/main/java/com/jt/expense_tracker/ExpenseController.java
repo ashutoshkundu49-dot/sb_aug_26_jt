2fbd8d5ba25d2593e9ef2f458a82f7c3d0d00eca
ashutoshkundu49-dot <ashutoshkundu49@gmail.com>
2026-09-02 21:15:24 +0530

    crud

--- expense-tracker/src/main/java/com/jt/expense_tracker/ExpenseController.java
+++ expense-tracker/src/main/java/com/jt/expense_tracker/ExpenseController.java
@@ -1,10 +1,12 @@
 package com.jt.expense_tracker;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class ExpenseController {
 
     private final JdbcTemplate jdbcTemplate;
+    private static final String EXPENSES_TABLE="expenses";
 
     // public ExpenseController(JdbcTemplate jdbcTemplate){
     //     this.jdbcTemplate=jdbcTemplate;
@@ -23,7 +26,7 @@ public class ExpenseController {
     
     @RequestMapping(value = "/expenses",method = RequestMethod.GET)
     public List<Expense> getExpenses(){
-            String sql="select * from expenses";
+            String sql="select * from %s".formatted(EXPENSES_TABLE);
 
             // List<Expense> expenses =new ArrayList<>();
 
@@ -49,6 +52,23 @@ public class ExpenseController {
             return jdbcTemplate.query((sql),new BeanPropertyRowMapper<Expense>(Expense.class)); 
             
 
+    }
+
+    @RequestMapping(value = "/expenses/{id}",method = RequestMethod.GET)
+    public Expense getExpenseById(@PathVariable int id){
+        // System.out.println("id is "+id);
+
+        String sql="select * from %s where id=?".formatted(EXPENSES_TABLE);
+      Expense expense=  jdbcTemplate.queryForObject
+      (sql, new BeanPropertyRowMapper<>(Expense.class),id);
+         return expense;
+    }
+
+    @RequestMapping(value = "/expenses",method = RequestMethod.POST)
+    public Expense createExpense(@RequestBody Expense expense){
+    var sql="insert into %s (title,category,price,date) values(?,?,?,?)".formatted(EXPENSES_TABLE);
+    jdbcTemplate.update(sql, expense.getTitle(),expense.getCategory(),expense.getPrice(),expense.getDate());
+        return expense;
     }
 
 }
