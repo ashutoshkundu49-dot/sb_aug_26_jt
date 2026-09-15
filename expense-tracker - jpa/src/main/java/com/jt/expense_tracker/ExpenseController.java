f84f50a3875ae7eab23e4e661ec85aae85bc31ea
ashutoshkundu49-dot <ashutoshkundu49@gmail.com>
2026-09-15 18:12:53 +0530

    chore:Move/expenses to a common prefix controller

--- expense-tracker - jpa/src/main/java/com/jt/expense_tracker/ExpenseController.java
+++ expense-tracker - jpa/src/main/java/com/jt/expense_tracker/ExpenseController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,23 +22,24 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("http://localhost:5173")
+@RequestMapping ("/expenses")
 public class ExpenseController {
 
     private final ExpenSeservice expenSeservice;
 
-    @GetMapping("/expenses")
+    @GetMapping
     public List<Expense> getExpenses() {
         return expenSeservice.getExpenses();
     }
 
-    @GetMapping("/expenses/{id}")
+    @GetMapping("/{id}")
     public Expense getExpenseById(@PathVariable int id) {
 
         return expenSeservice.getExpenseById(id);
 
     }
 
-    @PostMapping("/expenses")
+    @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Expense createExpense(@RequestBody Expense expense) {
         return expenSeservice.addExpense(expense);
@@ -45,7 +47,7 @@ public class ExpenseController {
         // return expenseRepository.save(expense);
     }
 
-    @DeleteMapping("/expenses/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteExpense(@PathVariable int id) {
 
@@ -53,7 +55,7 @@ public class ExpenseController {
 
     }
 
-    @PutMapping("/expenses")
+    @PutMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Expense updateExpense(@RequestBody Expense expense) {
 
